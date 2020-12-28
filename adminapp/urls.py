c477072b8da8cce8b0acbb8abc79d53c006ccbc5
Alex <sadofnik@bk.ru>
2020-12-28 13:12:39 +0300

    Выполнил

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -6,13 +6,13 @@ app_name = 'adminapp'
 
 urlpatterns = [
     path('', adminapp.index, name='index'),
-    path('users/', adminapp.admin_users, name='admin_users'),
-    path('users/create/', adminapp.admin_users_create, name='admin_users_create'),
-    path('users/update/<int:user_id>/', adminapp.admin_users_update, name='admin_users_update'),
-    path('users/delete/<int:user_id>/', adminapp.admin_users_delete, name='admin_users_delete'),
+    path('users/', adminapp.UserListView.as_view(), name='admin_users'),
+    path('users/create/', adminapp.UserCreateView.as_view(), name='admin_users_create'),
+    path('users/update/<int:pk>/', adminapp.UserUpdateView.as_view(), name='admin_users_update'),
+    path('users/delete/<int:pk>/', adminapp.UserDeleteView.as_view(), name='admin_users_delete'),
 
-    path('categories/', adminapp.admin_categories, name='admin_categories'),
-    path('categories/create/', adminapp.admin_categories_create, name='admin_categories_create'),
-    path('categories/update/<int:cat_id>/', adminapp.admin_categories_update, name='admin_categories_update'),
-    path('categories/delete/<int:cat_id>/', adminapp.admin_categories_delete, name='admin_categories_delete'),
+    path('categories/', adminapp.CategoriesListView.as_view(), name='admin_categories'),
+    path('categories/create/', adminapp.CategoriesCreateView.as_view(), name='admin_categories_create'),
+    path('categories/update/<int:pk>/', adminapp.CategoriesUpdateView.as_view(), name='admin_categories_update'),
+    path('categories/delete/<int:pk>/', adminapp.CategoriesDeleteView.as_view(), name='admin_categories_delete'),
 ]
